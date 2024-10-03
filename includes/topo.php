21a0a90e7cbca3276c39424d1f10d107ff8574d5
carololiveirapaes1 <carol.paes@aluno.ifasp.edu.br>
2024-10-03 11:20:03 -0300

    oih

--- includes/topo.php
+++ includes/topo.php
@@ -1,14 +1,16 @@
-<?php 
-session_start();
+<?php
+    session_start();
 ?>
+
 <div class="card">
-    <div clss="card-header">
-        <h1>Projeto Blog em PHP + MySQL IFSP - MURILO</h1>
+    <div class="card-header">
+        <h1> Projeto Blog em PHP + MySQL IFSP - Ratatouille'Greatest Fan (Arrieiro) </h1>
     </div>
-    <?php if(isset($_SESSION['login'])): ?>
-        <div class="card-body text_right">
-            OLá<?php echo $_SESSION['login']['usuario']['nome'] ?>! 
-            <a href="core/usuario_repositorio.php?acao=logout" class="btn btn-link btn-sm" role="button">Sair</a>
-        </div>
-        <?php endif ?>
+    <?php if (isset($_SESSION['login'])): ?>
+    <div class="card-body text-right">
+        Olá <?php echo $_SESSION['login']['usuario']['nome']?>!
+        <a href="core/usuario_repositorio.php?acao=logout"
+        class="btn btn-link btn-sm" role="button">Sair</a>
+    </div>
+    <?php endif ?>
 </div>
