0f472561e60e07fc800231b82b7d325abd2df48b
carololiveirapaes1 <carol.paes@aluno.ifasp.edu.br>
2024-10-03 12:07:13 -0300

    sjahjçw

--- includes/topo.php
+++ includes/topo.php
@@ -4,7 +4,7 @@
 
 <div class="card">
     <div class="card-header">
-        <h1> Projeto Blog em PHP + MySQL IFSP - Ratatouille'Greatest Fan (Arrieiro) </h1>
+        <h1> Projeto Blog em PHP + MySQL IFSP - Carol </h1>
     </div>
     <?php if (isset($_SESSION['login'])): ?>
     <div class="card-body text-right">
